5f9f1ee75aef051a4db061568965138c7fba8781
Fernando Mendoza <lfmendoza.alvarez@gmail.com>
2023-10-31 10:14:10 -0600

    feat: GPS Location

--- laboratorio_11/app/build.gradle.kts
+++ laboratorio_11/app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -67,5 +67,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("com.google.android.gms:play-services-location:17.8.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
--- laboratorio_11/app/src/main/java/com/example/laboratorio_11/MainActivity.kt
+++ laboratorio_11/app/src/main/java/com/example/laboratorio_11/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.laboratorio_11
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,15 +17,40 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.laboratorio_11.ui.theme.Laboratorio_11Theme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 
+
+var PERMISSION_ID = 42
 class MainActivity : ComponentActivity() {
 
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if(allPermissionsGrantedGPS())
+        {
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            leerUbicacionActual()
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ))
+        }
+
+
+
         setContent {
             Laboratorio_11Theme {
                 // A surface container using the 'background' color from the theme
@@ -30,6 +62,81 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun allPermissionsGrantedGPS()= REQUIRED_PERMISSIONS_GPS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun leerUbicacionActual() {
+        if(checkPermissions()) {
+            if(isLocationEnabled())
+                if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            ){
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    var location: Location? = task.result
+                    if(location == null) {
+                        requestNewLocationData()
+                    } else {
+                        binding.lbllatitud.text = "Latitud = " + location.latitude.toString()
+                        binding.lbllongitud.text = "Longitud = " + location.latitude.toString()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Activar ubicación", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+                this.finish()
+            }
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ))
+        }
+
+    }
+
+    private fun requestNewLocationData() {
+        var mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+    }
+
+    private fun mLocationCallback: Object = LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            var mLastLocation : Location = locationResult.lastLocation
+            binding.lbllatitud = "Latitud = " + mLastLocation.latitude.toString()
+            binding.lbllongitud = "Longitud = " + mLastLocation.longitude.toString()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun checkPermissions(): Boolean {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    companion object {
+        private val REQUIRED_PERMISION_GPS = arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION,
+            PERMISSION_ID
+        )
     }
 }
 
